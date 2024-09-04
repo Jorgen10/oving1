8274c66433f7d2fbf11a9ecb12916e7103f65fc3
Jorgen_10 <strabjorgen@gmail.com>
2024-09-04 12:38:29 +0200

    Added a try catch

--- python/oppgave-10.py
+++ python/oppgave-10.py
@@ -1,11 +1,14 @@
 def tallverdi():
-    tegn = input("Skriv inn en bokstav: ")
-    if len(tegn) < 1:
-        print("Du må skrive inn en bokstav!")
+    try:
+        tegn = input("Skriv inn en bokstav: ")
+        if len(tegn) < 1:
+            print("Du må skrive inn en bokstav!")
+            return tallverdi()
+        if len(tegn) > 1:
+            print("Du har for mange bokstaver!")
+            return tallverdi()
+        return ord(tegn)
+    except:
         return tallverdi()
-    if len(tegn) > 1:
-        print("Du har for mange bokstaver")
-        return tallverdi()
-    return ord(tegn)
 
 print(tallverdi())
